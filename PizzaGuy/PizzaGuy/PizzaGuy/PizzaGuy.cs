5ea2b5451ab18287ce699ebf69268444e859bc63
TheRealColbyMarquez <colbymarquez@msn.com>
2013-11-12 08:39:30 -0500

    11/12/13

---
+++ PizzaGuy/PizzaGuy/PizzaGuy/PizzaGuy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PizzaGuy
+{
+    class PizzaGuy: Sprite 
+    {
+        public PizzaGuy(
+            Vector2 location,
+            Texture2D texture,
+            Rectangle initialFrame,
+            Vector2 velocity)
+    }
+}
